2fda33cb06d6de2967fc212aa5b0245364110df7
爱吃糖葫芦 <13126506430@163.com>
2022-09-28 17:05:20 +0800

    feat: 初始化项目；

---
+++ OpenAPI2MD.CommunityToolkit/OpenAPIMDGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenAPI2MD.CommunityToolkit
+{
+
+    public class OpenAPIMDGenerator
+    {
+        public  string ReadYaml()
+        {
+            var lines=File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "openapi.yaml"));
+            return string.Empty;
+        }
+    }
+
+}
---
+++ OpenAPI2MD.CommunityToolkitTests/OpenAPIMDGeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenAPI2MD.CommunityToolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAPI2MD.CommunityToolkit.Tests
+{
+    [TestClass()]
+    public class OpenAPIMDGeneratorTests
+    {
+        [TestMethod()]
+        public void ReadYamlTest()
+        {
+            //Arrange 
+            var expected = new OpenAPIMDGenerator().ReadYaml();
+
+            //Act
+            var actual = "";
+
+            //Assert
+            Assert.AreEqual(expected, actual, "You are wrong!!!");
+
+        }
+    }
+}
